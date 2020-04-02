22922ae36b30ef2fe8e3d278d2220ad92260ef05
jjjliu1995 <46224518+jjjliu1995@users.noreply.github.com>
2020-04-01 21:40:56 -0400

    refactoring to function return shorthand for stateless functional components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <div>
-            <li>
-                {props.optionText}
-                <button 
-                    onClick={(e) => {
-                        props.handleDeleteOption(props.optionText);
-                    }}
-                >
-                    Delete Option
-                </button>
-            </li>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        <li>
+            {props.optionText}
+            <button 
+                onClick={(e) => {
+                    props.handleDeleteOption(props.optionText);
+                }}
+            >
+                Delete Option
+            </button>
+        </li>
+    </div>
+);
 
 export {Option as default};
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>
-                Remove All
-            </button>
-            <p>The number of options is {props.options.length}</p>
-            {props.options.length === 0 ?
-                <p>Please add an option to get started</p> :
-                (
-                    <div>
-                        <p>Your options are:</p>
-                        <ol>
-                            {props.options.map(option => {
-                                return <Option 
-                                        key={option}
-                                        optionText={option}
-                                        handleDeleteOption={props.handleDeleteOption}
-                                        />
-                            })}
-                        </ol>
-                    </div>
-                )
-            }
-        </div>
-    );
-};
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleDeleteOptions}>
+            Remove All
+        </button>
+        <p>The number of options is {props.options.length}</p>
+        {props.options.length === 0 ?
+            <p>Please add an option to get started</p> :
+            (
+                <div>
+                    <p>Your options are:</p>
+                    <ol>
+                        {props.options.map(option => {
+                            return <Option 
+                                    key={option}
+                                    optionText={option}
+                                    handleDeleteOption={props.handleDeleteOption}
+                                    />
+                        })}
+                    </ol>
+                </div>
+            )
+        }
+    </div>
+);
 
 export default Options
